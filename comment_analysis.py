27fc16741f9f7017f0e19a9f713a8e042bd5314c
Sinuhe Gonzalez <el-sinu>
2023-01-28 20:39:41 -0600

    wip reading csv into a dict so I can iterate

--- comment_analysis.py
+++ comment_analysis.py
@@ -2,32 +2,32 @@ import csv
 from csv import DictReader
 from textblob import TextBlob 
 
-filename = 'comments_copy.csv'
+filename = 'comment_test.csv'
 column_name = 'Comment'
 
-#Define Function to give sentiment scores using textblob
+########Define Function to give sentiment scores using textblob
 def sentiment(comment):
     processed_text = TextBlob(comment)
     polarity = processed_text.polarity
     return int(polarity)
+###########################################################
+
+########## Trying to do this via reading CSV instead #########
+
+file_handle = open(filename, "r", encoding="utf8")
+csv_reader = DictReader(file_handle)
+#Error: object of type 'DictReader' has no len(), because DictReader is an iterator
+#convert to list instead.
+# KeyError: 'Comment_ID'
+comments = list(csv_reader)
 
 final = []
-comments = [
-    {'Comment_ID': 0, 'Comment': 'This sucks!!'},
-    {'Comment_ID': 2, 'Comment': 'Everything is wonderful'},
-    {'Comment_ID': 3, 'Comment': 'This video is the best, I love it'},
-    {'Comment_ID': 4, 'Comment': "Woah, I can't believe it"},
-    {'Comment_ID': 5, 'Comment': "Meh, I'm indiferent about this"},
-    {'Comment_ID': 6, 'Comment': 'Cool beans'},
-    {'Comment_ID': 1, 'Comment': 'I hate my life'}
-    ]
-
 for i in range(len(comments)):
     comment = comments[i]
     final.append({
-        'Comment_ID': comment["Comment_ID"], 
-        'Comment': comment["Comment"],
-        'sentiment_score': sentiment(comment["Comment"])
+        "Comment_ID": comment["Comment_ID"], 
+        "Comment": comment["Comment"],
+        "Polarity": sentiment(comment["Comment"])
     })
 
 print(final)
