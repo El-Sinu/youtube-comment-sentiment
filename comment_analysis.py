6d7aff5477caaaba4e9e25e8cb53199505ebca86
Sinuhe Gonzalez <el-sinu>
2023-01-28 19:43:13 -0600

    exporting scores to txt file

--- comment_analysis.py
+++ comment_analysis.py
@@ -2,16 +2,17 @@ import csv
 from csv import DictReader
 from textblob import TextBlob 
 
-#filename = 'Comments_Latino_MidYear_Translated.csv'
+
 filename = 'comments_copy.csv'
-#column_name = 'translated'
 column_name = 'Comment'
 
 #This Works
 with open(filename, 'r', encoding='ISO-8859-1') as csvfile:
     csv_dict_reader = DictReader(csvfile)
-    for row in csv_dict_reader:
-        #Iterate over specific columns by column name or column number (index??)
-        processed_text = TextBlob(row[column_name])
-        polarity = processed_text.polarity
-        print(polarity)
+
+    with open('sentiment_scores.txt','w') as f:
+        for row in csv_dict_reader:
+            #Iterate over specific columns by column name or column number (index??)
+            processed_text = TextBlob(row[column_name])
+            polarity = processed_text.polarity
+            f.write(f"{(polarity)}\n")
