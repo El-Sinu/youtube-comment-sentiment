8224ac21553d12be7a0f7ebdb46ea913f66d442a
Sinuhe Gonzalez <el-sinu>
2023-01-28 20:19:18 -0600

    dictionary approach

--- comment_analysis.py
+++ comment_analysis.py
@@ -2,17 +2,32 @@ import csv
 from csv import DictReader
 from textblob import TextBlob 
 
-
 filename = 'comments_copy.csv'
 column_name = 'Comment'
 
-#This Works
-with open(filename, 'r', encoding='ISO-8859-1') as csvfile:
-    csv_dict_reader = DictReader(csvfile)
+#Define Function to give sentiment scores using textblob
+def sentiment(comment):
+    processed_text = TextBlob(comment)
+    polarity = processed_text.polarity
+    return int(polarity)
 
-    with open('sentiment_scores.txt','w') as f:
-        for row in csv_dict_reader:
-            #Iterate over specific columns by column name or column number (index??)
-            processed_text = TextBlob(row[column_name])
-            polarity = processed_text.polarity
-            f.write(f"{(polarity)}\n")
+final = []
+comments = [
+    {'Comment_ID': 0, 'Comment': 'This sucks!!'},
+    {'Comment_ID': 2, 'Comment': 'Everything is wonderful'},
+    {'Comment_ID': 3, 'Comment': 'This video is the best, I love it'},
+    {'Comment_ID': 4, 'Comment': "Woah, I can't believe it"},
+    {'Comment_ID': 5, 'Comment': "Meh, I'm indiferent about this"},
+    {'Comment_ID': 6, 'Comment': 'Cool beans'},
+    {'Comment_ID': 1, 'Comment': 'I hate my life'}
+    ]
+
+for i in range(len(comments)):
+    comment = comments[i]
+    final.append({
+        'Comment_ID': comment["Comment_ID"], 
+        'Comment': comment["Comment"],
+        'sentiment_score': sentiment(comment["Comment"])
+    })
+
+print(final)
